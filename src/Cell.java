9a0028fb0c47971da153772101e98352ca6f30ad
LewisYoon <trexhoon1212@gmail.com>
2026-07-27 18:43:16 +1000

    adds cell class and initialise game board grid

---
+++ src/Cell.java
@@ -0,0 +1,42 @@
+public class Cell {
+    private boolean mine;
+    private boolean selected;
+    private int mineCount;
+
+    // Constructor creates an empty cell
+    public Cell() {
+        mine = false;
+        selected = false;
+        mineCount = 0;
+    }
+
+    // Check if this cell has a mine
+    public boolean isMine() {
+        return mine;
+    }
+
+    // Set mine status
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    // Check if this cell is selected
+    public boolean isSelected() {
+        return selected;
+    }
+
+    // Reveal this cell
+    public void select() {
+        selected = true;
+    }
+
+    // Get surrounding mine count
+    public int getMineCount() {
+        return mineCount;
+    }
+
+    // Set surrounding mine count
+    public void SetMineCount(int count) {
+        mineCount = count;
+    }
+}
--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,9 +1,11 @@
 public class GameBoard {
     private int size;
+    private Cell[][] cells;
 
     // Constructor for board size
     public GameBoard(int size) {
         this.size = size;
+        createCells();
 
     }
 
@@ -30,4 +32,16 @@ public class GameBoard {
 
         System.out.println();
     }
+
+    // Create Cell objects for every position
+    private void createCells() {
+        cells = new Cell[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+    }
+
 }
