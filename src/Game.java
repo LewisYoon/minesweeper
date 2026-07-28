5f0875e636d69e871143db01ad1fa6f752d09925
LewisYoon <trexhoon1212@gmail.com>
2026-07-28 16:12:49 +1000

    adds bonus 1, user setups boarder size and number of mines

--- src/Game.java
+++ src/Game.java
@@ -7,15 +7,12 @@ public class Game {
 
     // Constructor creates game
     public Game() {
-        board = new GameBoard(10);
         scanner = new Scanner(System.in);
-        Mine mine = new Mine(board, 10);
-        mine.placeMine();
-        mine.calculateMineCount();
     }
 
     // Start game loop
     public void start() {
+        setupGame();
         while (true) {
             board.displayBoard();
             int i;
@@ -33,7 +30,7 @@ public class Game {
                 System.out.println("Invalid coordinate. Please enter between 1 and " + board.getSize());
                 continue;
             }
-            if (board.selectCell(i, j)) {
+            if (!board.selectCell(i, j)) {
                 board.displayBoard();
                 System.out.println("Boom!");
                 break;
@@ -46,10 +43,43 @@ public class Game {
         }
     }
 
+    // Setup game configuration from user input
+    private void setupGame() {
+        int size;
+        int mines;
+
+        while (true) {
+            try {
+                System.out.print("Enter board size (e.g 5 = 5x5 grid): ");
+                size = scanner.nextInt();
+
+                System.out.print("Enter number of mines: ");
+                mines = scanner.nextInt();
+
+                if (size <= 0) {
+                    System.out.println("Board size must be greater than 0.");
+                    continue;
+                }
+
+                if (mines <= 0 || mines >= size * size) {
+                    System.out.println("Mine count must be greater than 0 and less than total cells.");
+                    continue;
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter numbers only.");
+                scanner.nextLine();
+            }
+        }
+        board = new GameBoard(size);
+        Mine mine = new Mine(board, mines);
+        mine.placeMine();
+        mine.calculateMineCount();
+    }
+
     // Check if coordinate is inside the board
     private boolean checkCoordinate(int i, int j) {
         int size = board.getSize();
         return i >= 0 && i < size && j >= 0 && j < size;
     }
-
 }
